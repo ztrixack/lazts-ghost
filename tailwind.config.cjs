944a1d438f7e8687319495eb6c6d7cc2c1bdef52
Tanawat Hongthai <ztrixack.th@gmail.com>
2024-03-13 16:43:40 +0700

    chore: rollback tailwind to v3.4.1

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   darkMode: 'class',
   content: ['./src/app.html', './src/**/*.{js,ts,svelte}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [require('@tailwindcss/typography')],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()],
+	plugins: [sveltekit()],
+  define: {
+    'process.env': process.env,
+  },
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
